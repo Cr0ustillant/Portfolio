0dc08dc23315e3b6f63ebddb33aef05cd0c1c1bb
Jerôme Duprez <jeromeduprez@outlook.fr>
2024-07-15 15:04:55 +0200

    Finition

--- src/components/Cards/Card.jsx
+++ src/components/Cards/Card.jsx
@@ -4,7 +4,6 @@ function Card({ title , description , imgSrc , skill }) {
             <figcaption>
                 <h3>{title}</h3>
                 <p>{description}</p>
-                <p>{language}</p>
             </figcaption>
             <div className="wrapper">
                 <div className="triangle"></div>
